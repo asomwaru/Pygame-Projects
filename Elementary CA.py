1788877fe119b89047f41ec5653272c5920312a1
asomwaru <asomwaru01@gmail.com>
2020-06-30 01:04:36 -0400

    Found a huge bug that hasn't been fixed yet.

--- Elementary CA.py
+++ Elementary CA.py
@@ -1,13 +1,4 @@
-RULES = [1,0,0,1,0,1,1,0][::-1]
-
-def bin2dec(num:str):
-    total = 0
-
-    for i,x in enumerate(num[::-1]):
-        if x == '1':
-            total += (2**i)
-
-    return total
+RULES = [0,1,0,1,1,0,1,0][::-1]
 
 def check_rules(curr_arr:list) -> list:
     new_arr = [0 for _ in range(len(curr_arr))]
@@ -18,9 +9,8 @@ def check_rules(curr_arr:list) -> list:
         right = (x + 1) % (len(curr_arr))
 
         bits = [curr_arr[left], curr_arr[center], curr_arr[right]]
-        index = bin2dec("".join([str(y) for y in bits]))
+        index = int("".join([str(y) for y in bits]), 2)
 
-        print("".join([str(y) for y in bits]), index)
         new_arr[x] = RULES[index]
 
     return new_arr
@@ -29,12 +19,17 @@ def display_board(arr:list):
     for x in arr:
         print(" ".join(['#' if y == 1 else '.' for y in x]))
 
+def write_to_text(arr:list):
+    with open('triangle.txt', 'w') as fh:
+        for x in arr:
+            fh.write(" ".join(['#' if y == 1 else '.' for y in x]) + '\n')
+
 def main():
-    size = 25
+    size = 75
     triangle = [[0]*size]
     triangle[0][int(size/2)] = 1
 
-    for x in range(15):
+    for x in range(size):
         current = check_rules(triangle[x])
         triangle.append(current)
 
