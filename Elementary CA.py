bc11ee09036cc5817f26f488edbd1318b35b5a57
asomwaru <asomwaru01@gmail.com>
2020-07-07 06:38:06 -0400

    Added pygame element and I think I fixed the bug.

--- Elementary CA.py
+++ Elementary CA.py
@@ -1,39 +1,86 @@
-RULES = [0,1,0,1,1,0,1,0][::-1]
+import pygame
 
-def check_rules(curr_arr:list) -> list:
-    new_arr = [0 for _ in range(len(curr_arr))]
+RULES = [0,0,0,1,1,1,1,0][::-1]
+CELLS = 50
+board = [[0] * CELLS]
+GEN = CELLS
 
-    for x in range(len(curr_arr)):
-        left = (x - 1) % (len(curr_arr))
-        center = x
-        right = (x + 1) % (len(curr_arr))
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 
-        bits = [curr_arr[left], curr_arr[center], curr_arr[right]]
-        index = int("".join([str(y) for y in bits]), 2)
+SCALE = 10
+WIDTH = CELLS * SCALE
+HEIGHT = GEN * SCALE
+WINDOW_SIZE = [HEIGHT, WIDTH]
+FRAME_RATE = 10
 
-        new_arr[x] = RULES[index]
+print("Window Size: " + ", ".join(list(map(str, WINDOW_SIZE))))
 
-    return new_arr
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption("Elementary Cellular Automata")
 
-def display_board(arr:list):
-    for x in arr:
+def generate():
+    for y in range(GEN):
+        new_arr = [0] * CELLS
+        current = board[y]
+
+        for x in range(CELLS):
+            left = (x - 1) % (len(current))
+            center = x
+            right = (x + 1) % (len(current))
+
+            binary = int("".join(map(str, [current[left], current[center], current[right]])), 2)
+            new_arr[x] = RULES[binary]
+
+        board.append(new_arr)
+
+def display_board():
+    for x in board:
         print(" ".join(['#' if y == 1 else '.' for y in x]))
 
-def write_to_text(arr:list):
-    with open('triangle.txt', 'w') as fh:
-        for x in arr:
-            fh.write(" ".join(['#' if y == 1 else '.' for y in x]) + '\n')
+def draw():
+    for r in range(CELLS):
+        for c in range(GEN):
+            color = None
+
+            if board[r][c] == 1:
+                color = BLACK
+            elif board[r][c] == 0:
+                color = WHITE
+
+            pygame.draw.rect(
+                screen,
+                color,
+                [
+                    SCALE * c,
+                    SCALE * r,
+                    SCALE,
+                    SCALE,
+                ],
+            )
+
 
 def main():
-    size = 75
-    triangle = [[0]*size]
-    triangle[0][int(size/2)] = 1
+    done = False
 
-    for x in range(size):
-        current = check_rules(triangle[x])
-        triangle.append(current)
+    board[0][CELLS // 2] = 1
+    generate()
+    display_board()
 
-    display_board(triangle)
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+        screen.fill((0, 0, 0))
+        draw()
+
+        clock.tick(FRAME_RATE)
+
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     main()
